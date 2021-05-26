a324c02d3ae92a3b5ccf84f3610f522b00aec038
Vinicius Leal <56416018+viniciusleal34@users.noreply.github.com>
2021-05-26 17:31:38 -0300

    fix: envio do json

--- src/controllers/GithubController.js
+++ src/controllers/GithubController.js
@@ -7,20 +7,25 @@ module.exports = {
       if (language === "C") {
         language = "C#";
       }
+      let objeto = {};
       const { data } = await axios.get(
         `https://api.github.com/orgs/takenet/repos?page=1&per_page=100&language=${language}&order=desc`
       );
+
       const result = data
         .filter((repo, index) => repo.language === language)
+        .slice(0, 5)
         .map((repo, index) => {
-          return {
+          objeto["repo" + index] = {
             name: repo.name,
             description: repo.description,
             avatar_url: repo.owner.avatar_url,
           };
-        });
+          return objeto;
+        })
+        .slice(0, 1);
 
-      return res.status(200).json(result.slice(0, 5));
+      return res.status(200).json(result[0]);
     } catch (err) {
       console.log(err);
       return res
